22929500a9e38415a0d30cce8e8bc9e3aa2ed259
“Agares1024” <2025575070@qq.com>
2022-12-05 20:06:04 +0800

    Site updated: 2022-12-05 20:06:00

---
+++ dist/music.js
@@ -0,0 +1,22 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        name: "Ready?",
+        artist: 'D.D.D./塞壬唱片-MSR',
+        url: 'http://music.163.com/song/media/outer/url?id=1396561141.mp3',
+        cover: 'https://gitee.com/Agares1024/img/raw/master/img/9837e74acb0fd9a9ecfc485717176b3a-2022-12-411:49:20.jpg',
+    },
+    {
+        name: "Old Tales Preserved 海螺中的回音",
+        artist: '陈致逸/HOYO-MiX',
+        url: 'http://music.163.com/song/media/outer/url?id=1861403394.mp3',
+        cover: 'https://gitee.com/Agares1024/img/raw/master/img/ba59cf8d39d198f5c1b0def2b9076b1c-2022-12-411:51:01.jpg',
+    }
+    ]
+
+    
+
+    });
